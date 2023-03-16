24f576481c481dc608bc8dc7f7c00faeba1373d1
pgarcia86 <pgarciabarros86@gmail.com>
2023-03-17 00:34:39 +0100

    Ejercicio 2 terminado

---
+++ S04T01N02GarciaBarrosPablo/src/main/java/cat/itacademy/barcelonactiva/GarciaBarros/Pablo/s04/t01/n02/controller/HelloWorldController.java
@@ -0,0 +1,24 @@
+package cat.itacademy.barcelonactiva.GarciaBarros.Pablo.s04.t01.n02.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+	
+	@GetMapping("/HelloWorld")
+	public String saluda(@RequestParam(name="name", required=false, defaultValue="UNKNOWN") String name, Model model) {
+		model.addAttribute("name", name);
+		return "saludo";
+	}
+	
+	@GetMapping("/HelloWorld2/{name}")
+	public String saluda2(@PathVariable String name, Model model) {
+		model.addAttribute("name", name);
+		return "saludo2";
+	}
+
+}
