11c096f2837e3cdcadc450f5e34bfa25fa93fb75
pgarcia86 <pgarciabarros86@gmail.com>
2023-03-16 22:01:40 +0100

    Ejercicio 1 Terminado

---
+++ S04T01N01GarciaBarrosPablo/src/main/java/cat/itacademy/barcelonactiva/GarciaBarros/Pablo/s04/t01/n01/S04T01N01GarciaBarrosPabloApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.GarciaBarros.Pablo.s04.t01.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01GarciaBarrosPabloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01GarciaBarrosPabloApplication.class, args);
+	}
+
+}
---
+++ S04T01N01GarciaBarrosPablo/src/main/java/cat/itacademy/barcelonactiva/GarciaBarros/Pablo/s04/t01/n01/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package cat.itacademy.barcelonactiva.GarciaBarros.Pablo.s04.t01.n01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HelloWorldController {
+	
+	@GetMapping("/HelloWorld")
+	public String saluda(@RequestParam(name="name", required=false, defaultValue="UNKNOWN") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+	@GetMapping("/HelloWorld2/{name}")
+	public String saluda2(@PathVariable String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting2";
+}
+
+}
